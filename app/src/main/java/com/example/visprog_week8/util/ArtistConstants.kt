0200ebd16cd2b53b2bef8945422b4e596553ba65
felicia-sw <felicia.sword@gmail.com>
2025-11-01 15:20:20 +0700

    api changed

--- app/src/main/java/com/example/visprog_week8/util/ArtistConstants.kt
+++ app/src/main/java/com/example/visprog_week8/util/ArtistConstants.kt
@@ -2,5 +2,5 @@ package com.example.visprog_week8.util
 
 object ArtistConstants {
     // Base URL for TheAudioDB Public API.
-    const val BASE_URL = "https://www.theaudiodb.com/api/v1/json/1/"
+    const val BASE_URL = "https://www.theaudiodb.com/api/v1/json/123/"
 }
